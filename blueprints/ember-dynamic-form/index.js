5c3861288bed12a4a5564995516e3ff672d08a15
Phil Stott <phil.stott@gmail.com>
2016-11-14 23:45:15 +0000

    moved ember-truth-helpers to dependencies

--- blueprints/ember-dynamic-form/index.js
+++ blueprints/ember-dynamic-form/index.js
@@ -1,6 +1,6 @@
 /*jshint node:true*/
 module.exports = {
-  description: ''
+  description: '',
 
   // locals: function(options) {
   //   // Return custom template variables here.
@@ -9,7 +9,10 @@ module.exports = {
   //   };
   // }
 
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
+  afterInstall: function() {
+    return this.addBowerPackagesToProject([
+        { name: 'font-awesome' }
+    ]);
+  }
+
 };
